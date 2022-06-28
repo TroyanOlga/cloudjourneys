841c6f7e8254861ca7d8579cff1110fd722f57d4
TroyanOlga <lurryhurry@gmail.com>
2022-06-28 21:00:48 +0200

    initial commit

---
+++ index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+const app = express();
+const port = process.env.PORT;
+const host = process.env.HOST;
+
+// define a route handler for the default home page
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+// start the Express server
+app.listen(port, () => {
+  console.log(`server started at ${host}:${port}`);
+});
